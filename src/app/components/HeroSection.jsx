c3a7ee233f0f1c5bcce9e6f5b41d22f6c7c7f50e
noeysoftware <noeysoftware@gmail.com>
2024-07-18 18:25:20 +0700

    Update Contact me

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -36,7 +36,9 @@ const HeroSection = () => {
           </p>
           <div>
             <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
-              Contact Me
+              <a href="https://www.linkedin.com/in/suthassa-t-398688265/">
+                Contact Me
+              </a>
             </button>
             <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
